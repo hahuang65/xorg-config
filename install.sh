0c4470827a878f74244826ea029e2a0f75b97830
Howard Huang <hao@hwrd.me>
2018-11-20 15:50:12 -0800

    Correctly setup DPI for monitors

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ if [ $(uname) = "Linux" ]; then
   ln -sf "${PWD}/xinitrc" "${HOME}/.xinitrc"
   ln -sf "${PWD}/Xresources" "${HOME}/.Xresources"
   ln -sf "${PWD}/Xmodmap" "${HOME}/.Xmodmap"
+  sudo ln -sf "${PWD}/internal-monitor.conf" "/etc/X11/xorg.conf.d/90-monitor.conf"
+  sudo ln -sf "${PWD}/external-monitor.conf" "/etc/X11/xorg.conf.d/91-monitor.conf"
 fi
